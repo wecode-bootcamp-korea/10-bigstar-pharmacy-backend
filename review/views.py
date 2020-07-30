a875df862239cf9ee000744a991f247d9a46bc7a
Sangjun <0607juny@gmail.com>
2020-07-30 18:27:43 +0900

    리뷰 엔드포인트 수정

--- review/views.py
+++ review/views.py
@@ -8,18 +8,16 @@ from .models import Review
 class ReviewToPage(View):
     def get(self, request):
         reviews = Review.objects.all()
-        data_set=[]
 
         if request.method == "GET":
-            for i in  range(len(reviews)):
-                main_data = ({'id':reviews[i].id,
+            data_set =[ ({'id':reviews[i].id,
                               'name':reviews[i].name,
                               'purchased_item':reviews[i].purchased_item,
                               'purchased_date':reviews[i].purchased_date,
                               'photo':reviews[i].photo,
                               'comment':reviews[i].comment
-                             })
-                data_set.append(main_data)
+                             }) for i in range(len(reviews))]
+        
         return JsonResponse({'main_data':data_set})
 
 
