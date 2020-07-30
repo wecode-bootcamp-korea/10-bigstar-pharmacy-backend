0e20d731cec1a5b257302a5db83ecf14a192ae2c
Sangjun <0607juny@gmail.com>
2020-07-30 21:34:00 +0900

    Except 이하 에러메시지 아닌 값 준 부분 수정

--- product/views.py
+++ product/views.py
@@ -17,7 +17,7 @@ class ProductMainView(View):
             token   = request.headers.get('Authorization', None)
             payload = jwt.decode(token, SECRET_KEY, ALGORITHM)
             user    = User.objects.get(id = payload['user'])
-            if request.method == 'GET':
+            if token:
                 for i in range(len(info)):
                     n = i+1
                     print(user)
@@ -30,12 +30,12 @@ class ProductMainView(View):
                         cart = 0
 
                     main_data = ({'item':info[i].item,
-                              'decoration':info[i].decoration,
+                              'deco':info[i].deco,
                               'icon1':info[i].icon1,
                               'icon2':info[i].icon2,
-                              'description1':info[i].description1,
-                              'description2':info[i].description2,
-                              'description3':info[i].description3,
+                              'desc1':info[i].desc1,
+                              'desc2':info[i].desc2,
+                              'desc3':info[i].desc3,
                               'period':info[i].period,
                               'price':info[i].price,
                               'image':info[i].image_product,
@@ -43,27 +43,28 @@ class ProductMainView(View):
                               'cart':cart})
                     data_set.append(main_data)
             
-            return JsonResponse({'main_data':data_set})
+                return JsonResponse({'main_data':data_set})
         
-        except:
-            #if request == "GET":
-            for i in range(len(info)):
-                main_data = ({'item':info[i].item,
-                              'decoration':info[i].decoration,
+            else:
+                for i in range(len(info)):
+                    main_data = ({'item':info[i].item,
+                              'deco':info[i].deco,
                               'icon1':info[i].icon1,
                               'icon2':info[i].icon2,
-                              'description1':info[i].description1,
-                              'description2':info[i].description2,
-                              'description3':info[i].description3,
+                              'desc1':info[i].desc1,
+                              'desc2':info[i].desc2,
+                              'desc3':info[i].desc3,
                               'period':info[i].period,
                               'price':info[i].price,
                               'image':info[i].image_product,
                               'colors':info[i].colors,
                               'cart':0})
-                data_set.append(main_data)
+                    data_set.append(main_data)
 
-            return JsonResponse({'main_data':data_set})
-
+                return JsonResponse({'main_data':data_set})
+        
+        except KeyError:
+            return JsonResponse({'message':'KEY_ERROR'})
 
 class ProductDetailView(View):
     def get(self, request, id):
@@ -73,25 +74,26 @@ class ProductDetailView(View):
             token   = request.headers.get('Authorization', None)
             payload = jwt.decode(token, SECRET_KEY, ALGORITHM)
             user    = User.objects.get(id = payload['user'])
-            if Cart.objects.filter(product_id=id, user_id=user.id ).exists():
-                cart_info = Cart.objects.get(product_id=id, user_id=user.id)
-                cart      = cart_info.product_id
-             else:
-                cart = 0
-            num        = id-1
+            if token:
+                if Cart.objects.filter(product_id=id, user_id=user.id ).exists():
+                    cart_info = Cart.objects.get(product_id=id, user_id=user.id)
+                    cart      = cart_info.product_id
+                else:
+                    cart = 0
+                num        = id-1
         
-            item       = product[num].item
-            deco       = product[num].decoration
-            icon1      = info[num].icon1
-            icon2      = info[num].icon2
-            explan     = info[num].explanation
-            period     = info[num].period
-            price      = info[num].price
-            product_id = info[num].product_id
-            img_url    = info[num].image_detail
-            rest       = info[num].rest
+                item       = product[num].item
+                deco       = product[num].deco
+                icon1      = info[num].icon1
+                icon2      = info[num].icon2
+                explan     = info[num].explanation
+                period     = info[num].period
+                price      = info[num].price
+                product_id = info[num].product_id
+                img_url    = info[num].image_detail
+                rest       = info[num].rest
         
-            return JsonResponse({'item':item,
+                return JsonResponse({'item':item,
                                  'deco':deco,
                                  'icon1':icon1,
                                  'icon2':icon2,
@@ -102,21 +104,21 @@ class ProductDetailView(View):
                                  'img_url':img_url,
                                  'rest':rest,
                                  'cart':cart})
-        except:
-            num        = id-1
+            else:
+                num        = id-1
         
-            item       = product[num].item
-            deco       = product[num].decoration
-            icon1      = info[num].icon1
-            icon2      = info[num].icon2
-            explan     = info[num].explanation
-            period     = info[num].period
-            price      = info[num].price
-            product_id = info[num].product_id
-            img_url    = info[num].image_detail
-            rest       = info[num].rest
+                item       = product[num].item
+                deco       = product[num].deco
+                icon1      = info[num].icon1
+                icon2      = info[num].icon2
+                explan     = info[num].explanation
+                period     = info[num].period
+                price      = info[num].price
+                product_id = info[num].product_id
+                img_url    = info[num].image_detail
+                rest       = info[num].rest
         
-            return JsonResponse({'item' :item,
+                return JsonResponse({'item' :item,
                                  'deco' :deco,
                                  'icon1':icon1,
                                  'icon2':icon2,
@@ -127,4 +129,6 @@ class ProductDetailView(View):
                                  'img_url':img_url,
                                  'rest':rest,
                                  'cart':0})
+        except KeyError:
+            return JsonResponse({'message':'KEY_ERROR'}, status=401)
 
