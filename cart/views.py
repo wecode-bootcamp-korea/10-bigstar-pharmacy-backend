c4086b5c2d88062f85405179bae692e7aef593e5
Sangjun <0607juny@gmail.com>
2020-07-30 23:05:06 +0900

    장바구니앱 중복구문 삭제

--- cart/views.py
+++ cart/views.py
@@ -12,20 +12,18 @@ class CartAdd(View):
     @LoginConfirm
     def post(self, request):
         data = json.loads(request.body)
-        info = Product.objects.all()
         try:
             p_id = Product.objects.get(item=data['item']).id
-            user = User.objects.get(id = request.user.id)
             if Cart.objects.filter(item=data['item'], user_id=request.user.id).exists():
                 return JsonResponse({'message':'이미 담겼어요'})
-            else:
-                Cart(item      = data['item'],
-                    count      = data['count'],
-                    product_id = p_id,
-                    user_id    = user.id
-                ).save()
+            
+            Cart(item      = data['item'],
+                count      = data['count'],
+                product_id = p_id,
+                user_id    = request.user.id
+            ).save()
 
-                return JsonResponse({'message':'Added'})
+            return JsonResponse({'message':'Added'})
         
         except:
             return JsonResponse({'message':'품목이 잘못되었어요'})
@@ -50,14 +48,13 @@ class CartView(View):
         cart_in  = Cart.objects.filter(user_id=request.user.id)
         if not cart_in:
             return JsonResponse({'message':'장바구니에 추가된 제품이 없습니다. 몇가지 건강 설문을 통해 나만을 위한 영양성분을 찾아보세요.'})
-        elif request.method == 'GET':
-            data_set = [({'name' :cart_in[i].item,
-                          'count':cart_in[i].count,
-                          'id'   :cart_in[i].product_id,
-                          'image':Product.objects.get(id=cart_in[i].product_id).image_product,
-                          'price':Product.objects.get(id=cart_in[i].product_id).pricenum,
-                          'colors':Product.objects.get(id=cart_in[i].product_id).colors
-                          }) for i in range(len(cart_in))]
+        data_set = [({
+            'name' :cart_in[i].item,
+            'count':cart_in[i].count,
+            'id'   :cart_in[i].product_id,
+            'image':Product.objects.get(id=cart_in[i].product_id).image_product,
+            'price':Product.objects.get(id=cart_in[i].product_id).pricenum,
+            'colors':Product.objects.get(id=cart_in[i].product_id).colors}) for i in range(len(cart_in))]
 
         return JsonResponse({'order':data_set})
 
@@ -70,8 +67,7 @@ class DeleteItem(View):
             Cart.objects.filter(user_id=request.user.id).delete()
             return JsonResponse({'message':'텅텅'})
         
-        else:
-            return JsonResponse({'message':'delete을 입력해서 보내주세요'})    
+        return JsonResponse({'message':'delete을 입력해서 보내주세요'})    
 
 
 
