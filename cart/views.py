1c04325528dbe588145093074408a4087c74e044
Sangjun <0607juny@gmail.com>
2020-07-30 17:37:37 +0900

    장바구니 보기, 장바구니 비우기 엔드포인트 수정

--- cart/views.py
+++ cart/views.py
@@ -48,21 +48,16 @@ class CartView(View):
     @LoginConfirm
     def get(self, request):
         cart_in  = Cart.objects.filter(user_id=request.user.id)
-        data_set = []
         if not cart_in:
             return JsonResponse({'message':'장바구니에 추가된 제품이 없습니다. 몇가지 건강 설문을 통해 나만을 위한 영양성분을 찾아보세요.'})
         elif request.method == 'GET':
-            for i in range(len(cart_in)):
-                imgs  = cart_in[i].product_id
-                img   = Product.objects.get(id=imgs)
-                order = ({'name' :cart_in[i].item,
+            data_set = [({'name' :cart_in[i].item,
                           'count':cart_in[i].count,
                           'id'   :cart_in[i].product_id,
-                          'image':img.image_product,
-                          'price':img.pricenum,
-                          'colors':img.colors
-                          })
-                data_set.append(order)
+                          'image':Product.objects.get(id=cart_in[i].product_id).image_product,
+                          'price':Product.objects.get(id=cart_in[i].product_id).pricenum,
+                          'colors':Product.objects.get(id=cart_in[i].product_id).colors
+                          }) for i in range(len(cart_in))]
 
         return JsonResponse({'order':data_set})
 
@@ -74,7 +69,9 @@ class DeleteItem(View):
         if data['delete'] == 'delete':
             Cart.objects.filter(user_id=request.user.id).delete()
             return JsonResponse({'message':'텅텅'})
-
+        
+        else:
+            return JsonResponse({'message':'delete을 입력해서 보내주세요'})    
 
 
 
